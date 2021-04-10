b8300d41e8a5aaa7e32e5f4a5ebd36b9a672dad5
Daniel Asay <danieljasay@gmail.com>
2021-04-09 22:41:22 -0700

    add input options

--- mavs_automation.py
+++ mavs_automation.py
@@ -3,16 +3,40 @@ from selenium import webdriver
 
 #check the status of the mavericks game
 
-## take input argument 
+## take input argument (perhaps a speech intake)
 
-driver = webdriver.Chrome('/Users/dasay/website_login/chromedriver')  # Optional argument, if not specified will search path.
+## give commands without running script?
+
+def get_inquiry():
+	inq = ''
+	while True:
+		inq = input('What\'s your mavs question? (score, standings, injuries: ') 
+		if inq == 'score' or 'standings' or 'injuries':
+			return inq
+
+		else:
+			print("Input not valid! Please type score, standings or injuries")
+			continue
+
+inquiry = get_inquiry()
+
+driver = webdriver.Chrome('/Users/dasay/website_login/chromedriver')
 driver.get('http://www.google.com/');
-time.sleep(1) # Let the user actually see something!
+time.sleep(1)
 search_box = driver.find_element_by_name('q')
-search_box.send_keys('mavs game')
+search_box.send_keys("mavs ",inquiry)
 search_box.submit()
-#time.sleep() # Let the user actually see something!
-#driver.quit()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
