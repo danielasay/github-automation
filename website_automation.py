238158e7f959f2060568b97fdfda9858cfdd06f6
Daniel Asay <danieljasay@gmail.com>
2021-04-09 21:58:10 -0700

    mavs game

--- website_automation.py
+++ website_automation.py
@@ -1,14 +1,16 @@
 import time
 from selenium import webdriver
 
+#check the status of the mavericks game
+
 driver = webdriver.Chrome('/Users/dasay/website_login/chromedriver')  # Optional argument, if not specified will search path.
 driver.get('http://www.google.com/');
-time.sleep(5) # Let the user actually see something!
+time.sleep(1) # Let the user actually see something!
 search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
+search_box.send_keys('mavs game')
 search_box.submit()
-time.sleep(5) # Let the user actually see something!
-driver.quit()
+#time.sleep() # Let the user actually see something!
+#driver.quit()
 
 
 
